213d0100a12518e675cee282e4faaa783cf9f29a
matdelaterra <jor_am07@hotmail.com>
2020-05-18 21:05:22 -0500

    Modificación en la que se actualiza la función para importar nitratos, se agrega el directorio de output. Pendientes: crear función para manejar las coordenadas y documentación

--- dbnitratos.py
+++ dbnitratos.py
@@ -4,23 +4,25 @@ Created on Thu Mar 26 17:04:14 2020
 
 @author: Jorge Antonio Matías López
 """
-import numpy as np
+
 import pandas as pd
 from pathlib import Path
 
 def lista(ruta = Path.cwd()):
-    '''Función que sirve para obtener una lista con los nombres
+    '''Función que sirve para obtener una lista con los nombres,
        de los archivos contenidos en una carpeta 
     '''
     return [arch.name for arch in Path(ruta).iterdir() if arch.is_file()]
 
-def traslacion(df=None, x0=0, y0=0, dx=1, dy=1 ):
-    ''' Función que sirve para cambiar las coordenadas de UTM a coordenadas locales'''
-    if isinstance(df, pd.DataFrame):
-        df.loc[:, 'X'] -= x0
-        df.loc[:, 'Y'] = y0 - df.loc[:, 'Y']
     
-def importar_nitratos():
+def no3_xls2tob(MaxConcObs=200, MaxFluxObs=1, MaxFluxCells=0,#dataset1
+                outnam='prueba', inConcObs=21, inFluxObs=22, inSaveObs=23,#dataset2
+                Cscale=1.0, iOutCobs=1, iConcLOG=0, iConcINTP=1,#dataset3
+                COBSNAM='NO3', layer=4, weight=1, iComp=1,#dataset4
+                x0=455204.440, y0=2110063.17+64000, dx=2000, dy=2000, year_ini = 1934,#transformacion coordenadas
+                nFluxGroup=1, FScale=1, iOutFlux=1,#dataset6
+                nFluxTimeObs=1, ncells=0, iSSType=2,#dataset7
+                FluxTimeObs=0, weight_fobs=1, FluxObs=0):#dataset8
     
     #seleccionando archivos de excel
     nombres = []
@@ -32,22 +34,63 @@ def importar_nitratos():
     if len(nombres) != 0:
         
         conjunto_db = []
-        #instruccion para leer la geometría
-        
+        #instruccion para leer la geometría 
+         
         #DataFrame principal
         print('Importando archivos')
-        conjunto_db.append(pd.DataFrame({'NOMBRE':pd.Series([]),
-                              'CLAVE':pd.Series([]),
-                              'X':pd.Series([]), 
-                              'Y':pd.Series([]), 
-                              'NO3':pd.Series([])}))
         
         for nombre in nombres:
-            excel = pd.read_excel(Path.cwd() / 'input' /'nitratos'/ nombre)
-            excel = excel[['NOMBRE','CLAVE','X','Y','NO3']]
-            conjunto_db.append(excel)
+            df = pd.read_excel(Path.cwd()/ 'input'/'nitratos'/nombre,na_values=0)
+            df = df[['ALCALDIA','POZO','X','Y','NO3']]
+            cad = ''
+            for n in [int(s) for s in nombre if s.isdigit()]:
+                cad = cad + str(n)
+            df['YR'] = float(cad)
+            conjunto_db.append(df)
+        dataframe = pd.concat(conjunto_db, ignore_index=True)
         
-        return conjunto_db#pd.concat(conjunto_db)
+        nConcObs = dataframe['NO3'].count()
+        outnam = outnam + '.tob'
+        escritura = open(Path.cwd()/ 'output'/ outnam, 'w') 
+        header = '# TOB: Transport Observation package\n'
+        escritura.write(header)
+        
+        data_set1 = f'{MaxConcObs}\t{MaxFluxObs}\t{MaxFluxCells} # Data Set 1: MaxConcObs, MaxFluxObs, MaxFluxCells\n'
+        escritura.write(data_set1)
+        
+        data_set2 = f'{outnam}\t{inConcObs}\t{inFluxObs}\t{inSaveObs} # Data Set 2: OUTNAM, inConcObs, inFluxObs, inSaveObs\n'
+        escritura.write(data_set2)
+        
+        data_set3= f'{nConcObs}\t{Cscale:E}\t{iOutCobs}\t{iConcLOG}\t{iConcINTP}# Data Set 3: nConcObs, CScale, iOutCobs, iConcLOG, iConcINTP\n'
+        escritura.write(data_set3)
+        check = dataframe['NO3'].notnull()
+        
+        escritura.write('# Data Set 4: COBSNAM, Layer, Row, Column, iComp, TimeObs, Roff, Coff, weight, COBS\n')
+        for fila in range(dataframe.shape[0]):
+            if check[fila]:
+                COBS = dataframe['NO3'][fila]
+                xmod = dataframe['X'][fila] - x0
+                ymod = y0 - dataframe['Y'][fila]
+                row = ymod//dy + 1
+                column = xmod//dx + 1
+                r_off = (ymod - row*dy)/dy
+                c_off = (xmod - column*dx)/dx
+                #z, rho = geom(row,column) 
+                yr = dataframe['YR'][fila]
+                timeObs = (yr - year_ini) * 3600 * 24 * 365
+                COBS = dataframe['NO3'][fila]# dx*dy*z * rho * (1g/1000mg)
+                data_set4 = f'{COBSNAM}\t{layer}\t{int(row)}\t{int(column)}\t{iComp}  {timeObs:E}   {r_off:E}   {c_off:E}   {weight:E}   {COBS:E}\n'#datos del pozo
+                escritura.write(data_set4)
+        data_set6 = f'\t{nFluxGroup}  {FScale:E}   {iOutFlux} # Data Set 6: nFluxGroup, FScale, iOutFlux\n'
+        escritura.write(data_set6)
+        data_set7 = f'\t{nFluxTimeObs}    {ncells}    {iSSType} # Data Set 7: nFluxTimeObs, ncells, iSSType\n'
+        escritura.write(data_set7)
+        FOBSNAM=COBSNAM
+        data_set8 = f'{FOBSNAM}   {iComp}   {FluxTimeObs:E}   {weight_fobs:E}   {FluxObs:E} # Data set 8: FOBSNAM, iComp, FluxTimeObs, weight_fobs, FluxObs\n'
+        escritura.write(data_set8)
+        escritura.close()
+                
+        return dataframe
     else:
         print('No hay archivos para procesar en el directorio')
         
@@ -73,7 +116,7 @@ def xls2modflow(archivo=None, x0=0, y0=0, dx=2000, dy=2000, ml_obs=0, max_m=2, i
 '''
         
         formato = f'{numero_hobs} {ml_obs} {max_m} {iu_hobsv} {hob_dry:E}\n{tm_of_mult_hbs:.6f}\n'
-        escritura = open('datos.ob_hob', 'w')
+        escritura = open(Path.cwd()/ 'output'/'datos.ob_hob', 'w')
         escritura.write(encabezado)
         encabezado = '''#DATOS DE OBSERVACION
 #ENCABEZADO
@@ -125,9 +168,7 @@ def xls2modflow(archivo=None, x0=0, y0=0, dx=2000, dy=2000, ml_obs=0, max_m=2, i
         escritura.close()
     return datos
 
-
-
-datos = importar_nitratos()
+datos = no3_xls2tob()
 piez = xls2modflow('pozos_excel.xls',455204.44, 2110063.17+64000)
 
 
